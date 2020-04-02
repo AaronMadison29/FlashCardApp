039640c5f83b524ad62e8cdd4cd6c44213b82a1c
Aaron Madison <aaronmadison29@gmail.com>
2020-04-02 14:10:47 -0500

    Working on getting cards to flip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super(props);
+    this.state = {
+      isFlipped: false,
+      currentCard: 0,
+      data : [{'title':'Card 1', 'name':'Aaron'}, {'title':'Card 2', 'name':'Amanda'}, {'title':'Card 3', 'name':'Heather'}]
+    };
+  }
+  render(){
+    return(
+      <CardWindow 
+        isFlipped = {this.state.isFlipped}
+        currentCard = {this.state.currentCard}
+      />);
+  }
+}
+
+class CardWindow extends React.Component {
+
+  render() {
+    return (
+      <div className="row">
+        <Card
+        id= {this.props.currentCard}
+        isFlipped = {this.props.isFlipped}
+        />
+      </div>
+    );
+  }
+
+}
+
+function Card(props){
+  if (this.props.isFlipped) {
+    return (
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+          </div>
+        </div>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-back">
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
